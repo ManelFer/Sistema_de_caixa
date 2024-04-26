775c0787e1529d1d1c169cc9a33a3fb9703c6d35
Manoel Ferreira Matos <95552207+ManelFer@users.noreply.github.com>
2024-04-26 15:58:56 -0300

    Update Sistema_de_caixa.py
    Foi feita uma alteração. Adicionei funções e coloquei novos campos de criar conta e usuario. porem ta com falha

--- Sistema_de_caixa.py
+++ Sistema_de_caixa.py
@@ -1,16 +1,31 @@
-saldo_conta = 0
-saldo_banco = 200
-opcao = -1
+import textwrap
 
-def opcao1():
-    global saldo_conta, saldo_banco
+
+def menu():
+    print(
+        '''
+        ====== Sistema Bancario ======
+
+        1 - Depositar saldo na sua conta
+        2 - Sacar Saldo da sua conta
+        3 - Ver saldo da sua conta
+        4 - Novo usuario
+        5 - Nova Conta
+        6 - Lista contas
+        0 - Fechar programa
+
+        ====== Sistema Bancario ======
+        '''
+    )
+
+def depositar(saldo_conta):
     saldo_a_depositar = int(input("Qual o valor a ser depositado: "))
     saldo_conta += saldo_a_depositar
     print(f"Deposito de {saldo_a_depositar} feito com sucesso")
     print(f"Seu saldo atual é {saldo_conta}")
+    return saldo_conta
 
-def opcao2():
-    global  saldo_conta, saldo_banco
+def sacar(saldo_conta, saldo_banco, saldo_a_sacar):
     while True:
         try:
             saldo_a_sacar = int(input("Digite um valor para saque: "))
@@ -29,45 +44,92 @@ def opcao2():
         saldo_conta -= saldo_a_sacar
         print(f'Saque de {saldo_a_sacar} realizado com ucesso')
         print(f'Seu saldo atual é de {saldo_conta}')
-        
+    return saldo_conta
 
-def opcao3():
+def extrato(saldo_conta):
     print(f"Seu saldo atual é de {saldo_conta}\n")
 
-def opcao0():
-    print("Encerrado a cessão. Até mais tarde!")
+def novo_usuario(usuarios):
+    cpf = input("Digite seu cpf (Somente números): ")
+    usuario = filtro_usuario(cpf, usuarios)
+    if usuario:
+        print("Já existe um usuario")
+        return
+    nome = input("Qual o seu nome: ")
+    data_nascimento = input("Informe sua data de nascimento(dd-mm-aaaa): ")
+    endereco = input("Informe seu endereço (logadouro, nro - bairro - cidade): ")
 
-print(
-    '''
-    ====== Sistema Bancario ======
-
-    1 - Depositar saldo na sua conta
-    2 - Sacar Saldo da sua conta
-    3 - ver saldo da sua conta
-    0 - Fechar programa
-
-    ====== Sistema Bancario ======
-'''
-)
-
-while opcao != 0:
-    try:
-        opcao = int(input("Escolhar uma das opções: "))
-        if opcao not in [0, 1, 2, 3]:
-            print("opção invalida")
-            continue
-    except ValueError:
-        print("Opção invalida. Digite um número conforme o sistema indica")
-
-    if opcao == 1: opcao1()
-        
-    elif opcao == 2: opcao2()
-        
-    elif opcao == 3: opcao3()
-        
-    elif opcao == 0: opcao0()
-        
-    else:
-        print("Opção invalida, tentar novamente! \n")
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
+    print("Usuario criado com sucesso")
     
+def filtro_usuario(cpf, usuarios):
+    usuarios_filtros = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuarios_filtros[0] if usuarios_filtros else None
+
+def nova_conta(agencia, numero_conta, usuarios):
+    cpf = input("Informe CPF do cliente: ")
+    usuario = filtro_usuario(cpf, usuarios)
+    if usuario:
+        print("=== Conta criada com sucesso === ")
+        return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
+    print(" Usuario não encontrado, criação encerrada! ")
+
+def lista_contas(contas):
+    for conta in contas:
+        linha = f"""
+                    Agência: {conta['agencia']}
+                    c/c: {conta['numero_conta']}
+                    Titular: {conta['usuario']['nome']}
+                """
+        print("=" * 100)
+        print( textwrap.dedent(linha))
+
+def sair():
+    print("==== Encerrado a cessão. Até mais tarde! ====")
+
+def main():
+    AGENCIA = "0001"
+
+    saldo_conta = 0
+    saldo_banco = 200
+    extrato = ""
+    saldo_a_sacar = 0
+    usuarios = []
+    contas = []
+    opcao = -1
+
+    while opcao!= 0:
+        menu()
+        try:
+            opcao = int(input("Escolhar uma das opções: "))
+            if opcao not in [0, 1, 2, 3, 4, 5, 6]:
+                print("opção invalida")
+                continue
+        except ValueError:
+            print("Opção invalida. Digite um número conforme o sistema indica")
+
+        if opcao == 1: 
+            saldo_conta = depositar(saldo_conta)
+        elif opcao == 2: 
+            saldo_conta = sacar(saldo_conta, saldo_banco, saldo_a_sacar)
+        elif opcao == 3: 
+            extrato(saldo_conta)
+        elif opcao == 4:
+            novo_usuario(usuarios)
+        elif opcao == 5:
+            numero_conta = len(contas) + 1
+            conta = nova_conta(AGENCIA, numero_conta, usuarios)
+            if conta:
+                contas.append(conta)
+
+        elif opcao == 6:
+            lista_contas(contas)
+        elif opcao == 0: 
+            sair()
+        else:
+            print("Opção invalida, tentar novamente! \n")
+
+if __name__ == "__main__":
+    main()
+
     
